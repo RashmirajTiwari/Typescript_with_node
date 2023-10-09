bdcd00546c429d92654816c20a3db11cf0f1e430
Rashmiraj Tiwari <rashmirajt@athmin.com>
2023-10-09 17:15:01 +0530

    Integrating Typescript with Node and Express

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("Just started")
+import express from 'express';
+import todosRoutes from './routes/todos'
+import bodyParser from 'body-parser'
+
+const app=express();
+app.use(todosRoutes);
+app.use(bodyParser.json());
+app.listen({port:3000});
